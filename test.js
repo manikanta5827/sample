de01550e4b86c19b8588baadb9702dd9089ef85e
manikanta5827 <postbox5827@gmail.com>
2024-03-12 21:19:25 +0530

    first commit

--- test.js
+++ test.js
@@ -3,15 +3,15 @@ let fs = require("fs");
 
 http
   .createServer((req, res) => {
-    fs.writeFile('tes.txt'," sare",(err,data)=>{
-      if(err){
+    fs.writeFile("tes.txt", " sare", (err, data) => {
+      if (err) {
         throw err;
       }
-      
-    })
-    fs.readFile('tes.txt',(err,data)=>{
-      res.write(data)
-      res.end();
-    })
+    });
+    fs.readFile("tes.txt", (err, data) => {
+      res.write(data);
+
+      res.end("submitted succcessfully");
+    });
   })
   .listen(3000);
